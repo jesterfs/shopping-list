5ea694762d7fa14db0fd9b050bb77a44e4a02a8d
jesterfs <63027978+jesterfs@users.noreply.github.com>
2020-05-23 12:52:35 -0700

    actually fixed js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $('#js-shopping-list-form').on('submit', function(event){
   let text = $('#shopping-list-entry').val();
   $('.shopping-list').append("<li> <span class = 'shopping-item' >" + text + "</span> <div class = 'shopping-item-controls' ><button class = 'shopping-item-toggle' > <span class = 'button-label' > check </span> </button> <button class = 'shopping-item-delete' ><span class = 'button-label' > delete </span> </button> </div> </li>");
-  $('#shopping-list-entry').val();
+  // $('#shopping-list-entry').val();
   // $('#shopping-list-entry').val('');
   event.preventDefault();
 });
