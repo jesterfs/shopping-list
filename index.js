03f983193d8184664c6616299863fb8088f22ef6
jesterfs <63027978+jesterfs@users.noreply.github.com>
2020-05-23 12:47:42 -0700

    fixed js again
    added event delegation to delete function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 $('#js-shopping-list-form').on('submit', function(event){
   let text = $('#shopping-list-entry').val();
   $('.shopping-list').append("<li> <span class = 'shopping-item' >" + text + "</span> <div class = 'shopping-item-controls' ><button class = 'shopping-item-toggle' > <span class = 'button-label' > check </span> </button> <button class = 'shopping-item-delete' ><span class = 'button-label' > delete </span> </button> </div> </li>");
-
+  $('#shopping-list-entry').val();
+  // $('#shopping-list-entry').val('');
   event.preventDefault();
 });
 
 
 // $(document).ready(function () {
-  $('.shopping-item-toggle').click(function() {
+  $('ul').on('click', '.shopping-item-toggle' , function(event) {
     const target = $(this).closest('li').find('.shopping-item')
     if ($(target).is(".shopping-item__checked")){
       $(target).removeClass("shopping-item__checked");
